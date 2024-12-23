535906bf1fde37a2e238f51fda9c5c64d3db5033
Dutkercz <dutkercz@gmail.com>
2024-12-23 16:09:16 -0300

    New activities

--- untitled/src/Exercicios/Ex04/Pessoa/Pessoa.java
+++ untitled/src/Exercicios/Ex04/Pessoa/Pessoa.java
@@ -1,4 +1,46 @@
 package Exercicios.Ex04.Pessoa;
 
 public class Pessoa {
+
+    private String name;
+    private double height;
+    private int age;
+
+    public Pessoa(String name, int age, double eighth) {
+        this.name = name;
+        this.age = age;
+        this.height = eighth;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + getName() +
+                " Age: " + getAge() +
+                " Eight: " + getHeight()+"\n";
+    }
 }
